53be0a8c6ec9026304c34c882cae84598de5bc52
Furentsu <eugeniofranzoso@gmail.com>
2021-10-12 16:41:31 +0200

    Exercise completed

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <?php 
-    $myText = "PHP is a general-purpose scripting language geared towards web development.[7] It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994.[8] The PHP reference implementation is now produced by The PHP Group.[9] PHP originally stood for Personal Home Page,[8] but it now stands for the recursive initialism PHP: Hypertext Preprocessor.[10]
+    $my_text = "PHP is a general-purpose scripting language geared towards web development.[7] It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994.[8] The PHP reference implementation is now produced by The PHP Group.[9] PHP originally stood for Personal Home Page,[8] but it now stands for the recursive initialism PHP: Hypertext Preprocessor.[10]
 
     PHP code is usually processed on a web server by a PHP interpreter implemented as a module, a daemon or as a Common Gateway Interface (CGI) executable. On a web server, the result of the interpreted and executed PHP code – which may be any type of data, such as generated HTML or binary image data – would form the whole or part of an HTTP response. Various web template systems, web content management systems, and web frameworks exist which can be employed to orchestrate or facilitate the generation of that response. Additionally, PHP can be used for many programming tasks outside the web context, such as standalone graphical applications[11] and robotic drone control.[12] PHP code can also be directly executed from the command line.
     
@@ -20,11 +20,11 @@
 </head>
 <body>
     <p>
-        <?php echo $myText ?>
+        <?php echo str_replace($_GET['bad_word'], '***', $my_text); ?>
     </p>
 
     <h2> 
-        <?php echo "The above paragraph is " . strlen($myText) . " " . "characters long."; ?> 
+        <?php echo "The above paragraph is " . strlen($my_text) . " " . "characters long."; ?> 
     </h2>
 </body>
 </html>
